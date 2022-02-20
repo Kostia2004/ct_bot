ed60daff79602b1a25110de66ce29b08c9904cd7
Kostia2004 <k.a.kudriavsev@mail.ru>
2022-02-20 21:34:39 +0300

    read_nii function in Reader class

--- reader.py
+++ reader.py
@@ -1,25 +1,33 @@
 import nibabel as nib
 import gzip
+import numpy as np
+import os
+import shutil
 
 class Reader():
     def __init__(self):
         pass
 
     def read_nii(self, path):
-        pass
+        '''
+        Reads .nii file and returns pixel array
+        '''
+        ct_scan = nib.load(path)
+        array   = ct_scan.get_fdata()
+        array   = np.rot90(np.array(array))
+        return array
 
     def read_gzip(self, path):
         '''
         Reads .nii file from gzip and returns pixel array
         '''
-        with gzip.open(filepath, 'rb') as f_in:
-            with open(filepath[:-3]+'.nii', 'wb') as f_out:
+        with gzip.open(path, 'rb') as f_in:
+            with open(path[:-3]+'.nii', 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
-        ct_scan = nib.load(filepath[:-3]+'.nii')
+        ct_scan = nib.load(path[:-3]+'.nii')
         array   = ct_scan.get_fdata()
         array   = np.rot90(np.array(array))
-        os.remove(filepath[:-3]+'.nii')
-        os.remove(filepath)
+        os.remove(path[:-3]+'.nii')
         return array
 
     def read(self, path):
